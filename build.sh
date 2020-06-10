c9faaab89f242899cd74595f1faa83c59467db8c
fernnx2 <devfernando95@gmail.com>
2020-06-09 20:23:18 -0600

    modify: name image docker

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build -t "apachephpldap" .
+docker build -t "apachephpradius" .
 
-docker run --name=server1 -p 80:80 -it -d apachephpradius 
-docker run --name=server2 -p 81:80 -it -d apachephpradius
-docker run --name=server3 -p 82:80 -it -d apachephpradius
+docker run --name=serve1 -p 80:80 -it -d apachephpradius 
+docker run --name=serve2 -p 81:80 -it -d apachephpradius
+docker run --name=serve3 -p 82:80 -it -d apachephpradius
